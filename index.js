0211e4ddff77ec720a01099530c7bdbf98283623
Milan <milan.dhar@gmail.com>
2019-06-06 11:22:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(stringNames, eventName) {
+  let responseArray = []
+  for(let i = 0; i < stringNames.length; i++) {
+    responseArray.push(`Thank you, ${stringNames[i]}, for the wonderful surprise gift!`);
+  }
+  return responseArray;
+}
+
+
+function countdown(number) {
+  while(number > -1) {
+    console.log(number--);
+  }
+}
